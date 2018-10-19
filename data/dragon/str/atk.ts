24f301acf22b273f352cffb958f0846ff0faeba6
Nathan Grant <nathan.grant@gmail.com>
2018-10-19 18:43:45 -0400

    Auto-generated commit

---
+++ data/dragon/str/atk.ts
@@ -0,0 +1,189 @@
+[{ depth: 1, stat: "STR", points: 1330, spend: 0, 
+    left: 
+        { name: "Dark Slave", levels: 10, required: 6, stats: [
+            "Umbra element summoned by Ring of Element will be stronger and possesses 5% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 10% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 15% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 20% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 25% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 30% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 35% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 40% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 45% of your ATK"
+            ,"Umbra element summoned by Ring of Element will be stronger and possesses 50% of your ATK"
+            ]
+        },
+    center:
+        { name: "Fury of Divinity", levels: 5, required: 0, stats: [
+            "Normal attack will restore 1 more Rage when in human form"
+            ,"Normal attack will restore 2 more Rage when in human form"
+            ,"Normal attack will restore 3 more Rage when in human form"
+            ,"Normal attack will restore 4 more Rage when in human form"
+            ,"Normal attack will restore 5 more Rage when in human form"
+        ]},
+    right: 
+        { name: "Dragon Power", levels: 15, required: 9, stats: [
+            "Damage dealt by normal attack increases bt 10% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 20% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 30% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 40% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 50% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 60% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 70% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 80% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 90% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 100% when in dragon form" //10
+            ,"Damage dealt by normal attack increases bt 110% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 120% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 130% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 140% when in dragon form"
+            ,"Damage dealt by normal attack increases bt 150% when in dragon form"
+        ]}
+},
+{ depth: 2, stat: "STR", points: 1630, spend: 11,left: {
+    name: "Steal", levels: 10, required: 6, stats: [
+        "Attack of Umbra element grants a 12% chance to steal 1 class buff from the target" // 6
+
+    ]
+}, center: {}, right: {
+    name: "Bloody Bite (PvP)", levels: 5, required: 3, stats: [
+        "Converts 1% damage dealt to HP when in dragon form"
+        ,"Converts 2% damage dealt to HP when in dragon form"
+        ,"Converts 3% damage dealt to HP when in dragon form"
+        ,"Converts 4% damage dealt to HP when in dragon form"
+        ,"Converts 5% damage dealt to HP when in dragon form"
+    ]
+}},
+{ depth: 3, stat: "STR", points: 1930, spend: 18,left: {}, center: {
+    name: "Dragon Rage", levels: 15, required: 0, stats: [
+        "Changes Thunder Tempest effect to summoning dragon flame and deals 400% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 425% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 450% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 475% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 500% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 525% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 550% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 575% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 600% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 625% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 650% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 675% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 700% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 725% skill damage to 4 targets within the range (Also valid in PvP)"
+        ,"Changes Thunder Tempest effect to summoning dragon flame and deals 750% skill damage to 4 targets within the range (Also valid in PvP)"
+
+    ]
+}, right: {}},
+{ depth: 4, stat: "STR", points: 2180, spend: 30,left: {
+    name: "Umbra Guardian", levels: 10, required: 10, stats: [
+        "Each attack of Umbra element grants a 20% chance to increase your Block Rate by 5%. It can be stacked up to 3x and last for 3s."
+        ,"Each attack of Umbra element grants a 20% chance to increase your Block Rate by 5%. It can be stacked up to 3x and last for 3s."
+    ]
+}, center: {}, right: {
+    name: "Awakened Dragon", levels: 10, required: 10, stats: [
+        "Grants a 10% chance to transform directly if Rage maxes out when in human form"
+        ,"Grants a 10% chance to transform directly if Rage maxes out when in human form"
+    ]
+}},
+{ depth: 5, stat: "STR", points: 2430, spend: 40,left: {
+    name: "Umbra Despair", levels: 1, required: 1, stats: [
+        "Umbra element will explode when it disappears to deal 400% skill damage to surrounding players and disables them to regen HP for 2s"
+    ]
+}, center: {}, right: {
+    name: "Bloody Tempest (PvP)", levels: 1, required: 1, stats: [
+        "When Rage >50, Ignore Defense+100%, damage+200%, 30% damage will be converted to HP for 3s. CD: 60s"
+    ]
+}},
+{ depth: 6, stat: "STR", points: 2530, spend: 45,left: {
+    name: "Life Circle",
+    levels: 10,
+    required: 10,
+    stats: [
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+        ,"Every time the Element disappears, it has a 75% chance to summon another Element"
+    ]
+
+}, center: {
+    name: "Wing Master",
+    levels: 20,
+    required: 0,
+    stats: [
+        "When equipping Lv 4+ wings, ATK+0.5%"
+        ,"When equipping Lv 4+ wings, ATK+1%"
+        ,"When equipping Lv 4+ wings, ATK+1.5%"
+        ,"When equipping Lv 4+ wings, ATK+2%"
+        ,"When equipping Lv 4+ wings, ATK+2.5%"
+        ,"When equipping Lv 4+ wings, ATK+3%"
+        ,"When equipping Lv 4+ wings, ATK+3.5%"
+        ,"When equipping Lv 4+ wings, ATK+4%"
+        ,"When equipping Lv 4+ wings, ATK+4.5%"
+        ,"When equipping Lv 4+ wings, ATK+5%"
+        ,"When equipping Lv 4+ wings, ATK+5.5%"
+        ,"When equipping Lv 4+ wings, ATK+6%"
+        ,"When equipping Lv 4+ wings, ATK+6.5%"
+        ,"When equipping Lv 4+ wings, ATK+7%"
+        ,"When equipping Lv 4+ wings, ATK+7.5%"
+        ,"When equipping Lv 4+ wings, ATK+8%"
+        ,"When equipping Lv 4+ wings, ATK+8.5%"
+        ,"When equipping Lv 4+ wings, ATK+9%"
+        ,"When equipping Lv 4+ wings, ATK+9.5%"
+        ,"When equipping Lv 4+ wings, ATK+10%"
+    ]
+}, right: {
+    name: "Fatal Blow",
+    levels: 10,
+    required: 10,
+    stats: [
+        "If you trigger a critical attack when in human form, you can restore 0.5% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 1% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 1.5% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 2% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 2.5% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 3% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 3.5% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 4% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 4.5% HP"
+        ,"If you trigger a critical attack when in human form, you can restore 5% HP"
+        
+    ]    
+}},
+{ depth: 7, stat: "STR", points: 2570, spend: 60,left: {
+    name: "Umbra Bless",
+    levels: 10,
+    required: 0,
+    stats: [
+        ,"Every time the Element disappears, it has a 42% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 50% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 58% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 64% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 72% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 80% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 88% chance to trigger Damage Immunity 5x continuously"
+        ,"Every time the Element disappears, it has a 100% chance to trigger Damage Immunity 5x continuously"
+    ]
+}, center: {}, right: {
+    name: "Abyss Pillage (PvP)",
+    levels: 10,
+    required: 0,
+    stats: [
+        "Every time Dragon Realm hits the target, it will steal 3% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 4% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 5% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 6% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 7% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 8% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 9% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 10% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 11% of target's ATK and DEF to yourself for 5s. CD: 1s"
+        ,"Every time Dragon Realm hits the target, it will steal 12% of target's ATK and DEF to yourself for 5s. CD: 1s"
+    ]
+}}
+]
---
+++ data/dragon/str/def.ts
@@ -0,0 +1,200 @@
+[
+    { depth: 1, stat: "STR", points: 1330, spend: 0, left: {
+        name: "Energy Drain",
+        levels: 10,
+        required: 6,
+        stats: [
+            ,"When using Desperate Hope, ATK+2% over 5s"
+            ,"When using Desperate Hope, ATK+4% over 5s"
+            ,"When using Desperate Hope, ATK+6% over 5s"
+            ,"When using Desperate Hope, ATK+8% over 5s"
+            ,"When using Desperate Hope, ATK+10% over 5s"
+            ,"When using Desperate Hope, ATK+12% over 5s"
+            ,"When using Desperate Hope, ATK+14% over 5s"
+            ,"When using Desperate Hope, ATK+16% over 5s"
+            ,"When using Desperate Hope, ATK+18% over 5s"
+            ,"When using Desperate Hope, ATK+20% over 5s"
+
+        ]
+    }, center: {
+        name: "HP Absorb",
+        levels: 10,
+        required: 0,
+        stats: [
+            "Each attack of Ring of Blessing will restore 20% more HP"
+            ,"Each attack of Ring of Blessing will restore 40% more HP"
+            ,"Each attack of Ring of Blessing will restore 60% more HP"
+            ,"Each attack of Ring of Blessing will restore 80% more HP"
+            ,"Each attack of Ring of Blessing will restore 100% more HP"
+            ,"Each attack of Ring of Blessing will restore 120% more HP"
+            ,"Each attack of Ring of Blessing will restore 140% more HP"
+            ,"Each attack of Ring of Blessing will restore 160% more HP"
+            ,"Each attack of Ring of Blessing will restore 180% more HP"
+            ,"Each attack of Ring of Blessing will restore 200% more HP"
+        ]
+    }, right: {
+        name: "Stoneskin (PvP)",
+        levels: 10,
+        required: 6,
+        stats: [
+            ,"Grants a 1% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 2% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 3% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 4% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 5% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 6% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 7% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 8% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 9% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+            ,"Grants a 10% chance to increase your Block rate by 20% over 3s if your Rage is higher than 30 when in human form. CD: 60s"
+
+        ]
+    }}
+    , { depth: 2, stat: "STR", points: 1630, spend: 11, left: {
+        name: "Tyrant Dragon",
+        levels: 10,
+        required: 6,
+        stats: [
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 0.2s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 0.4s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 0.6s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 0.8s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 1.0s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 1.2s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 1.4s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 1.6s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 1.8s. Skill influence times+2"
+            ,"Increases duration time of Desperate Hope, Dragon Fury (or Dragon Tempest) by 2.0s. Skill influence times+2"
+        ]
+    }, center: {}, right: {
+        name: "Heavenly Shield",
+        levels: 10,
+        required: 6,
+        stats: [
+            "Each attack grants an 8% chance to trigger shield effect to increase your Block Rate by 70% over 2s" //10
+        ]
+    }}
+    , { depth: 3, stat: "STR", points: 1930, spend: 18, left: {}, center: {
+        name: "Wing Mastery",
+        levels: 20,
+        required: 0,
+        stats: [
+            "Equip Lv 3+ wings to increase Max HP 1.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 3.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 4.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 6.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 7.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 9.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 10.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 12.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 13.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 15.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 16.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 18.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 19.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 21.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 22.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 24.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 25.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 27.0%"
+            ,"Equip Lv 3+ wings to increase Max HP 28.5%"
+            ,"Equip Lv 3+ wings to increase Max HP 30.0%"
+        ]
+    }, right: {}}
+    , { depth: 4, stat: "STR", points: 2180, spend: 30, left: {
+        name: "Last Resort (PvP)",
+        levels: 10,
+        required: 10,
+        stats: [
+            "Under Morale Strike or Divine Blessing state, damage dealt+20%, every 1 surrounding enemy player will increase damage dealt by 5%. Can be increased up to 40%" //10
+        ]
+    }, center: {}, right: {
+        name: "Elf Divinity",
+        levels: 5,
+        required: 5,
+        stats: [
+            "Decreases CD for Ring of Divinity to dispel negative effects bt 25s"
+        ]
+    }}
+    , { depth: 5, stat: "STR", points: 2430, spend: 40, left: {
+        name: "Dragon Will (PvP)",
+        levels: 1,
+        required: 1,
+        stats: [
+            "If you receive deadly attack when in human form, your HP will be restored according to Rage (Every Rage grants 1% HP) and your Rage will be filled up. CD:90s"
+        ]
+    }, center: {}, right: {
+        name: "Shield Onslaught (PvP)",
+        levels: 1,
+        required: 1,
+        stats: [
+            "Each Block decreases Dragon Domain CD by 3s and restores HP that equals to 5% of Max HP."
+        ]
+    }}
+    , { depth: 6, stat: "STR", points: 2530, spend: 45, left: {
+        name: "Fury Surge (PvP)",
+        levels: 10,
+        required: 10,
+        stats: [
+            "Grants a 100% chance to get 5 Rage when being attacked in human form"
+        ]
+    }, center: {
+        name: "Wing Mastery",
+        levels: 20,
+        required: 0,
+        stats: [
+            "When equipping Lv 4+ wings, HP+1.5%"
+            ,"When equipping Lv 4+ wings, HP+3.0%"
+            ,"When equipping Lv 4+ wings, HP+4.5%"
+            ,"When equipping Lv 4+ wings, HP+6.0%"
+            ,"When equipping Lv 4+ wings, HP+7.5%"
+            ,"When equipping Lv 4+ wings, HP+9.0%"
+            ,"When equipping Lv 4+ wings, HP+10.5%"
+            ,"When equipping Lv 4+ wings, HP+12.0%"
+            ,"When equipping Lv 4+ wings, HP+13.5%"
+            ,"When equipping Lv 4+ wings, HP+15.0%"
+            ,"When equipping Lv 4+ wings, HP+16.5%"
+            ,"When equipping Lv 4+ wings, HP+18.0%"
+            ,"When equipping Lv 4+ wings, HP+19.5%"
+            ,"When equipping Lv 4+ wings, HP+21.0%"
+            ,"When equipping Lv 4+ wings, HP+22.5%"
+            ,"When equipping Lv 4+ wings, HP+24.0%"
+            ,"When equipping Lv 4+ wings, HP+25.5%"
+            ,"When equipping Lv 4+ wings, HP+27.0%"
+            ,"When equipping Lv 4+ wings, HP+28.5%"
+            ,"When equipping Lv 4+ wings, HP+30.0%"
+        ]
+    }, right: {
+        name: "Thunder Bluster",
+        levels: 10,
+        required: 10,
+        stats: [
+            "Every block grants a 30% chance to reset the CD of Thunder Slash"
+        ]
+    }}
+    , { depth: 7, stat: "STR", points: 2570, spend: 60, left: {
+        name: "Deadly Fury (PvP)",
+        levels: 10,
+        required: 0,
+        stats: [
+            "Triggers Control Res. for 5s when Rage is higher than 90. CD: 20s"
+        ]
+    }, center: {}, right: {
+        name: "Forbidden Force (PvP)",
+        levels: 10,
+        required: 0,
+        stats: [
+            "Every block grants a 20% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 26.67% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 33.33% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 40% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 46.67% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 53.33% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 60% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 66.67% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 73.33% chance to strike back the target and decreased target's HP by 15%"
+            ,"Every block grants a 80% chance to strike back the target and decreased target's HP by 15%"
+
+        ]
+    }}
+]
